912e7e537f067d057534222aa9a5061649aae077
nonolin1128 <taiyijob@gmail.com>
2023-12-22 23:36:12 +0800

    counter增加功能完成

--- next-base-main/pages/test04-05/test05.js
+++ next-base-main/pages/test04-05/test05.js
@@ -1,5 +1,20 @@
-import React from 'react'
+// 導入react中的使用狀態勾子
+
+import { useState } from 'react'
 
 export default function test05() {
-  return <div>test05</div>
+  // 宣告狀態
+  // [獲得值的變數, 設定值的函式] = useState(初始值)
+  const [total, setTotal] = useState(0)
+
+  return (
+    <h1
+      onClick={() => {
+        setTotal(total + 1)
+      }}
+      role="presentation"
+    >
+      {total}
+    </h1>
+  )
 }
