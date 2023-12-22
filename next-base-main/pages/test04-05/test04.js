3d1c0fd9265af6195ade9b5b461a0dda3091b3e6
nonolin1128 <taiyijob@gmail.com>
2023-12-22 23:45:43 +0800

    test04-done

--- next-base-main/pages/test04-05/test04.js
+++ next-base-main/pages/test04-05/test04.js
@@ -1,5 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function test04() {
-  return <div>test04</div>
+export default function Test04() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  return (
+    <div>
+      <h1>test04</h1>
+      <div>
+        {isLoggedIn ? (
+          <p>Welcome, you are logged in!</p>
+        ) : (
+          <p>Please log in to access the content.</p>
+        )}
+      </div>
+      <button onClick={() => setIsLoggedIn(!isLoggedIn)}>
+        Toggle Login Status
+      </button>
+    </div>
+  )
 }
